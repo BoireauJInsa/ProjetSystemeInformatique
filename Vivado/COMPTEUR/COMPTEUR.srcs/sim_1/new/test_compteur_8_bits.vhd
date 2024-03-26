6139c8ec04053cd92cb30df73080a58d7db93e53
unknown <boireau-@insa-toulouse.fr>
2024-03-26 12:51:45 +0100

    ajout compteur

---
+++ Vivado/COMPTEUR/COMPTEUR.srcs/sim_1/new/test_compteur_8_bits.vhd
@@ -0,0 +1,87 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/08/2024 11:26:33 AM
+-- Design Name: 
+-- Module Name: testBench_compteur - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_unsigned.ALL;
+use IEEE.std_logic_arith.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity testBench_compteur is
+
+end testBench_compteur;
+
+architecture bench of testBench_compteur is
+
+    component compteur_8_bits is
+        Port ( CK : in STD_LOGIC;
+               SENS : in STD_LOGIC;
+               EN : in STD_LOGIC;
+               RST : in STD_LOGIC;
+               LOAD : in STD_LOGIC;
+               Din : in STD_LOGIC_VECTOR (7 downto 0);
+               Dout : out STD_LOGIC_VECTOR (7 downto 0));
+    end component;
+  
+       signal sclk : STD_LOGIC;
+       signal ssens : STD_LOGIC;
+       signal sen : STD_LOGIC;
+       signal srst : STD_LOGIC;
+       signal sload : STD_LOGIC;
+       signal sdin : STD_LOGIC_VECTOR (7 downto 0);
+       signal sdout : STD_LOGIC_VECTOR (7 downto 0);
+       
+begin
+    compteur: compteur_8_bits PORT MAP (sclk,ssens,sen,srst,sload,sdin,sdout);
+    
+    prst: process
+    begin
+        sen <= '0';
+        ssens <= '0';
+        sdin <= (others => '1');    
+        sload <= '1' after 0 ns;
+        srst <= '1' after 0 ns;
+        sload <= '0' after 100 ns;
+        
+        wait for 100000 ns;
+    end process;
+        
+    pclk: process
+    begin
+        sclk <= '0';
+        wait for 7 ns;
+        sclk <= '1';
+        wait for 7 ns;
+     end process;
+     
+   
+        
+      
+
+end bench;
---
+++ Vivado/COMPTEUR/COMPTEUR.srcs/sources_1/new/compteur_8_bits.vhd
@@ -0,0 +1,59 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/08/2024 11:04:55 AM
+-- Design Name: 
+-- Module Name: compteur_8_bits - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_unsigned.ALL;
+use IEEE.std_logic_arith.ALL;
+
+
+entity compteur_8_bits is
+    Port ( CK : in STD_LOGIC;
+           SENS : in STD_LOGIC;
+           EN : in STD_LOGIC;
+           RST : in STD_LOGIC;
+           LOAD : in STD_LOGIC;
+           Din : in STD_LOGIC_VECTOR (7 downto 0);
+           Dout : out STD_LOGIC_VECTOR (7 downto 0));
+end compteur_8_bits;
+
+architecture Behavioral of compteur_8_bits is
+    signal count : std_logic_vector(7 downto 0);
+begin
+    process (CK,RST,LOAD,EN,Din,SENS,count)
+        begin
+        if (CK = '1' and CK'event) then
+            if (RST = '0') then
+                count <= (others => '0');
+            elsif (LOAD = '1' and EN = '0') then
+                count <= Din;
+            elsif (EN = '0') then
+                if (SENS = '1') then
+                    count <= count + 1;  
+                else 
+                    count <= count - 1; 
+                end if;
+            end if;
+        end if;
+    end process;
+    
+    Dout <= count;
+end Behavioral;
